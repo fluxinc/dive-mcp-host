4acd698435ace756f1f6598d1a07f76a7021c6b1
Louis <louis@funmula.com>
2025-03-17 13:14:03 +0800

    Define DiveUser.
    for user information and token usage.

--- dive_mcp_host/httpd/middlewares/general.py
+++ dive_mcp_host/httpd/middlewares/general.py
@@ -1,4 +1,5 @@
 from collections.abc import Callable
+from typing import TypedDict
 
 from starlette.requests import Request
 from starlette.responses import Response
@@ -25,7 +26,31 @@ async def error_handler(request: Request, call_next: Callable) -> Response:
         )
 
 
+class DiveUser(TypedDict):
+    """User-related state storage.
+
+    This state can be accessed by all middlewares and handlers.
+    """
+
+    user_id: str | None
+    user_name: str | None
+    user_type: str | None
+    token_spent: int
+    """The amount of tokens spent by the user in this period."""
+    token_limit: int
+    """The amount of tokens the user can use in this period."""
+    token_increased: int
+    """The amount of tokens increased in this request."""
+
+
 async def default_state(request: Request, call_next: Callable) -> Response:
     """Prefill default state."""
-    request.state.dive_user = {}
+    request.state.dive_user = DiveUser(
+        user_id=None,
+        user_name=None,
+        user_type=None,
+        token_spent=0,
+        token_limit=0,
+        token_increased=0,
+    )
     return await call_next(request)
